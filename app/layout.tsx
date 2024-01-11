0936f58c2d53c9fdbfdce67bb5dbda9d8ba5a2b3
Viren Khatri <vk102002@gmail.com>
2024-01-11 16:20:25 +0530

    fix: temp 3d fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body
         className={cn(
-          "max-w-screen-2xl mx-auto bg-background font-sans antialiased no-scrollbar",
+          "max-w-screen-2xl mx-auto bg-background font-sans antialiased",
           inter.variable
         )}
       >
